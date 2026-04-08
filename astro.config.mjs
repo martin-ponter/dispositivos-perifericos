d92c3fdbfe586e64989d411527f69b6154417fe6
altarialights <altarialights@gmail.com>
2026-04-08 10:50:52 +0200

    problema entre vercel y bitrix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import vercel from "@astrojs/vercel";
+import tailwindcss from "@tailwindcss/vite";
 
-import tailwindcss from '@tailwindcss/vite';
-
-import react from '@astrojs/react';
-
-
-// https://astro.build/config
 export default defineConfig({
-  server: {
-    allowedHosts: ['actively-churchmanly-joann.ngrok-free.dev']
-  },
+  output: "server",
+  adapter: vercel(),
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
-
-  integrations: [react()]
+  integrations: [react()],
 });
