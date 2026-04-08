b4ac9375848cbc9da497b9b5e98749a5f9869c2a
altarialights <altarialights@gmail.com>
2026-04-08 11:14:23 +0200

    problema entre vercel y bitrix

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,9 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   output: "server",
   adapter: vercel(),
+  security: {
+    checkOrigin: false,
+  },
   vite: {
     plugins: [tailwindcss()],
   },
